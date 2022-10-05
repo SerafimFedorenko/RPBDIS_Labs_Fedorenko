ee57f2024c73856970a027ea06196481832a161c
SerafimFedorenko <92511061+SerafimFedorenko@users.noreply.github.com>
2022-10-05 23:02:49 +0300

    .

---
+++ Lab3/Lab3/RecyclingPointLib/Models/Position.cs
@@ -0,0 +1,12 @@
+﻿namespace RecyclingPointLib.Models
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public override string ToString()
+        {
+            return (new { Код_должности = Id, Название = Name }).ToString();
+        }
+    }
+}

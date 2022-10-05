a5b90d91861e209dab887bbfd584612c4a7f8ac4
SerafimFedorenko <92511061+SerafimFedorenko@users.noreply.github.com>
2022-10-05 23:01:51 +0300

    .

---
+++ Lab2/RecyclingPointLib/Models/Storage.cs
@@ -0,0 +1,16 @@
+﻿namespace RecyclingPointLib.Models
+{
+    public class Storage
+    {
+        public int Id { get; set; }
+        public int StorageTypeId { get; set; }
+        public string? Name { get; set; }
+        public int Number { get; set; }
+        public int Square { get; set; }
+        public int Capacity { get; set; }
+        public int Occupancy { get; set; }
+        public int Depreciation { get; set; }
+        public DateTime CheckDate { get; set; }
+        public StorageType? StorageType { get; set; }
+    }
+}

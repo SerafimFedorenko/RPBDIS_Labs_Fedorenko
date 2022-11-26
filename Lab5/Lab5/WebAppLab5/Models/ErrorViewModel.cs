9163bb3976047d4e6d59e300b43dae42e20bd6fd
SerafimFedorenko <92511061+SerafimFedorenko@users.noreply.github.com>
2022-11-27 01:21:05 +0300

    .

--- Lab5/Lab5/WebAppLab5/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAppLab5.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
